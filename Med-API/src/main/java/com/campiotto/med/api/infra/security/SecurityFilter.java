b8f13f1d50dd01d2b68aeb6010b232bcfac17581
Guilherme Campiotto <guilherme_campiotto@hotmail.com>
2023-01-13 17:04:01 -0300

    Adding token validation, configuration for endpoints that don't need token (create user and login)

---
+++ Med-API/src/main/java/com/campiotto/med/api/infra/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.campiotto.med.api.infra.security;
+
+import com.campiotto.med.api.domain.user.UserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter  extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenJWT = getToken(request);
+
+        if(tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = userRepository.findByLogin(subject);
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader != null) {
+            return authHeader.replace("Bearer", "");
+        }
+
+        return null;
+
+    }
+}
--- Med-API/src/main/java/com/campiotto/med/api/infra/security/TokenService.java
+++ Med-API/src/main/java/com/campiotto/med/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.campiotto.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.campiotto.med.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +34,20 @@ public class TokenService {
 
         return token;
     }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Medical")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT invalid or expired");
+        }
+    }
+
 
     private Instant dateExpiry() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
